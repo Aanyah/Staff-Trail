55c2c567b731364e94adf7132203ea6f24c86169
CRUMBB <aanyahlc@gmail.com>
2022-03-12 22:12:42 -0500

    starting code/server.js code/sql creation

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES ();
+
+INSERT INTO  job (title, salary, department_id)
+VALUE ();
+
+INSERT INTO employee (first_name, last_name, job_id, manager_id)
+VALUE ();
